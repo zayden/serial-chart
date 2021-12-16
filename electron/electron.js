126256a9b9b51f7793982889b90cbaf084cb8813
zayden <zaydenwang@gmail.com>
2021-12-16 08:51:13 +0800

    增加election和串口支持

---
+++ electron/electron.js
@@ -0,0 +1,71 @@
+// electron/electron.js
+const SerialPort = require("serialport");
+
+const port = new SerialPort(
+  "COM1", {
+  baudRate: 115200, //波特率
+  dataBits: 8, //数据位
+  parity: 'none', //奇偶校验
+  stopBits: 1, //停止位
+  flowControl: false
+}, false);
+port.write('main screen turn on', function (err) {
+  if (err) {
+    return console.log('Error on write: ', err.message)
+  }
+  console.log('message written')
+})
+
+// Open errors will be emitted as an error event
+port.on('error', function (err) {
+  console.log('Error: ', err.message)
+})
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+const isDev = process.env.IS_DEV == "true" ? true : false;
+
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+    },
+  });
+
+  // and load the index.html of the app.
+  // win.loadFile("index.html");
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../dist/index.html')}`
+  );
+  // Open the DevTools.
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
